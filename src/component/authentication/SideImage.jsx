e372ba7eb6a8d8bd5daaa64a774af26204535924
Tajbir23 <studenttajbirislam@gmail.com>
2024-09-05 14:15:41 +0600

    done: signup

--- src/component/authentication/SideImage.jsx
+++ src/component/authentication/SideImage.jsx
@@ -8,6 +8,7 @@ const SideImage = () => {
             <img src="/vite.svg" alt="logo" className="m-auto" />
             <div className="text-center">
             <h1 className="text-white text-[40px] font-bold">Furni<span className="text-blue-400">Flex</span></h1>
+            <p className="text-white text-[16px] font-medium mx-28">Discover a seamless shopping experience with our curated collection of products. From fashion to electronics, we bring quality.</p>
             </div>
         </div>
     </div>
